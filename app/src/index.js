0f024b1b174f2b4a33a0b99a93224e7731b1c988
Vlad <vovaturcan4@gmail.com>
2019-03-11 20:49:19 +0200

    renaming to components

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./blah/App";
+import App from "./components/App";
 
 // import redux stuff
 import { createStore, applyMiddleware, compose } from "redux";
